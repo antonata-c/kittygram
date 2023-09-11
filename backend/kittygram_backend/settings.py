2b1b325aca859069c1ab5d512358c8315bfa25a9
Anton <anton_ata@mail.ru>
2023-09-11 22:36:00 +0300

    fixed ports

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'NO_KEY')
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '158.160.17.220']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
